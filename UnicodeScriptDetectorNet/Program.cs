1cfb27bb26188414fa92fffe554851e31f22b203
Dani Kempinski <dkempinski@gmail.com>
2019-09-26 19:28:33 +0300

    test

--- UnicodeScriptDetectorNet/Program.cs
+++ UnicodeScriptDetectorNet/Program.cs
@@ -6,7 +6,7 @@ namespace UnicodeScriptDetectorNet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); 
         }
     }
 }
