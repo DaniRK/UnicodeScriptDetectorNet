e5c41c72162730d63f7b801165b16af41483ae2a
Dani Kempinski <dani@globalcre.net>
2019-09-26 19:02:35 +0300

    Add project files.

---
+++ UnicodeScriptDetectorNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnicodeScriptDetectorNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
